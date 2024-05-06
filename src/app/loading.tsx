d052bd3254fbcf5d7306610a5c327c0f9a5c91db
Jacky <jacky@iyes.dev>
2024-05-06 16:53:26 +0800

    chore: remove useless css

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,5 @@
-import Loading from "@/components/atoms/Loading";
+import LoadingArea from "@/components/molecules/LoadingArea";
 
 export default function RootLoading() {
-  return (
-    <div className="w-full h-full flex justify-center items-center">
-      <Loading />
-    </div>
-  );
+  return <LoadingArea />;
 }
